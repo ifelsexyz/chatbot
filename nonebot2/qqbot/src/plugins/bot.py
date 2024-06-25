4284457f98585bb3fb9b30147c58e04368312a56
user <user@debian.debian>
2024-06-24 21:38:38 -0400

    1 commit

---
+++ nonebot2/qqbot/src/plugins/bot.py
@@ -0,0 +1,28 @@
+from nonebot import on_keyword, on_message
+from nonebot.rule import to_me
+from nonebot.adapters import Bot, Event
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import GroupMessageEvent,Bot,Message,MessageSegment,Event
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyCzTgoCUzbJ60sVEYCgDG1QlUaKxCov-vg")
+
+model = genai.GenerativeModel('gemini-1.5-pro')
+
+chat = on_message(rule=to_me(), priority=5)
+@chat.handle()
+async def chat_handle(bot: Bot, event: Event, state: T_State):
+    msg = str(event.message)
+    response = model.generate_content(msg)
+
+    msg_list =[]
+    msg_list.append(
+        {
+            "type": "node",
+            "data": {
+                "name": "1",
+                "uin": event.self_id,
+                "content": response.text
+                }
+            }
+        )
+    await bot.send_group_forward_msg(group_id = event.group_id, messages = msg_list)
